ba6166eff1fcb2fb1411ecd2dcc29b4eed947d14
2321564369 <128552900+2321564369@users.noreply.github.com>
2025-12-01 16:53:57 -0500

    Add files via upload

---
+++ bluey/Смотреть Блуи (Bluey, 2018, Сериал - 1 сезон) на английском с субтитрами онлайн в хорошем качестве Full HD 720 и 1080_files/dle_filter.js.php
@@ -0,0 +1,25 @@
+let dleFilter = {
+	path: window.location.pathname,
+	title: document.title,
+	content: false,
+	speedbar: false,
+	reset: false,
+	id: '',
+	original: {},
+	ajax: 0,
+	lazy: 1,
+	ajaxUrl: 1,
+	hideLoading: 1,
+	button: 0,
+	jsSelect: 0,
+	ajaxNav: 1,
+	ajaxPage: 1,
+	ajaxAnim: 0,
+	ionSlider: 0,
+	navApart: 0,
+	filterUrl: 'f',
+	defaultUrl: '/f/sort=date/order=desc/'
+};
+
+
+
